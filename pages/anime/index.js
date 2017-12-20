366a0ad5ca001b7aeaeec81ec4d246d0b48e7f9b
plain <dingxh@boqii.com>
2017-12-20 18:37:43 +0800

    提交更新

--- pages/anime/index.js
+++ pages/anime/index.js
@@ -5,9 +5,88 @@ Page({
    * 页面的初始数据
    */
   data: {
-    anime: []
+    anime: [],
+    playList: [],
+    playerList: [],
+    playerData: [],
+    close: false
   },
-
+  vodInfo: function (event) {
+    const id = event.currentTarget.dataset.id;
+    wx.navigateTo({
+      url: `../anime/index?id=${id}`
+    })
+  },
+  playName(a) {
+    var b = []
+    b['tudou'] = '土豆'
+    b['youku'] = '优酷'
+    b['iqiyi'] = '爱奇艺'
+    b['letv'] = '乐视'
+    b['sohu'] = '搜狐'
+    b['pptv'] = '聚力'
+    b['qq'] = '腾讯'
+    b['letvyun'] = '乐视云'
+    b['bilibili'] = '哔哩哔哩'
+    b['acfun'] = 'A站'
+    b['other'] = '其他'
+    b['pv'] = 'PV'
+    b['bgm'] = 'BGM'
+    b['ed'] = 'ED'
+    b['cm'] = 'CM'
+    b['op'] = 'OP'
+    b['mad'] = 'MAD'
+    b['yugao'] = '预告'
+    return b[a]
+  },
+  playUrl(type, url) {
+    console.log(url)
+    return `http://www.ikanfan.com${url}?s=${type}&f=xcx`
+  },
+  playerList(event) {
+    const dataset = event.currentTarget.dataset;
+    const { vid, url } = dataset
+    const d1 = url.split('/')[3].split('-')
+    const d2 = d1[1].split('.')
+    const sid = d1[0]
+    const pid = d2[0]
+    console.info(this)
+    const data = this.data.playList
+    let playerData = []
+    for (let e = 0; e < data.length; e++) {
+      const d = data[e].playurls[parseInt(pid) - 1]
+      pid <= data[e].playurls.length && playerData.push({ type: data[e].playname, vid: d[1], name: this.playName(data[e].playname), title: d[0], url })
+    }
+    console.info(playerData, sid, pid)
+    this.setData({
+      playerData,
+      close: true
+    })
+  },
+  player(event) {
+    const dataset = event.currentTarget.dataset;
+    const { type, url } = dataset
+    const href = this.playUrl(type, url)
+    wx.setClipboardData({
+      data: href,
+      success: function (res) {
+        wx.getClipboardData({
+          success: function (res) {
+            wx.showModal({
+              title: '复制成功',
+              content: `把《${href}》粘贴到浏览器就可以播放了`,
+              showCancel: false
+            })
+          }
+        })
+      }
+    })
+  },
+  close() {
+    this.setData({
+      close: false
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -15,12 +94,23 @@ Page({
     console.info(this, options)
     const that = this
     wx.request({
-      url: `https://www.ikanfan.cn/tool/xcxDetail.php?id=${options.id}`, //仅为示例，并非真实的接口地址
+      url: `https://www.ikanfan.cn/tool/xcxDetail.php?id=${options.id}`, // 详情
       success: function (res) {
         console.log(res.data.data[0])
-        console.info(this)
         that.setData({
           anime: res.data.data[0],
+        })
+        wx.setNavigationBarTitle({ title: res.data.data[0].name })
+      }
+    })
+    wx.request({
+      url: `https://www.ikanfan.cn/tool/xcxplaylist.php?id=${options.id}`, // 播放列表
+      success: function (res) {
+        console.log(res.data.data[0])
+        const data = res.data.data.Data
+        that.setData({
+          playList: data,
+          playerList: data[0].playurls
         })
       }
     })
